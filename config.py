df6387789a49d7566e0bcb96fc9e4ddb9fd4b3b1
michaelbuckner <mike@mikebuckner.me>
2019-07-09 11:16:26 -0400

    first commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 INSTANCE_CONFIG = {
     # Instance should be without full URL. Just instance name
-    'instance': 'ubsshowcasedemo',
-    'username': 'admin',
-    'password': 'Professi0n@l'
+    'instance': '',
+    'username': '',
+    'password': ''
 }
